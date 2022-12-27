1611d684b0754468372e1c958fd2e58ebf15d154
Abhinav0909 <abhinavkarforma2@gmail.com>
2022-12-27 20:00:51 +0530

    ✨ Add features of ultimate to do

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import React, { useState } from "react";
 function App() {
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [triggerTask, setTriggerTask] = useState(false);
+  const [isAnyCompletedTask, setIsAnyCompletedTask] = useState(false);
+  const addData = () => {
+    if (input === null || input === "") {
+      alert("Need to type something");
+      return;
+    }
+    const newToDo = {
+      id: new Date().getTime(),
+      text: input,
+      completed: false,
+    };
+    setTodos((todo) => {
+      const updatedList = [...todo].concat(newToDo);
+      console.log(updatedList);
+      setInput("");
+      return updatedList;
+    });
+  };
+  function removeData(key) {
+    const updatedList = todos.filter((todo) => todo.id !== key);
+    setTodos(updatedList);
+  }
+  const handleCheckBox = (key) => {
+    let isCheckBoxClicked = false;
+    todos.forEach((todo) => {
+      if (todo.id === key) {
+        todo.completed = !todo.completed;
+      }
+      if (todo.completed) {
+        isCheckBoxClicked = true;
+      }
+    });
+    setTodos([...todos]);
+    setIsAnyCompletedTask(isCheckBoxClicked);
+  };
+  const handleComplete = () => {
+    setTriggerTask(true);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1 className="title">To do app</h1>
+      <div className="search">
+        <input
+          type="search"
+          placeholder="Enter your task here"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        ></input>
+        <button onClick={addData}>Add</button>
+      </div>
+      {todos.map((todo) => (
+        <div key={todo.id} className="list">
+          <div className="todolist">{todo.text}</div>
+          <div>
+            <input
+              className="checkbox"
+              type="checkbox"
+              checked={todo.completed}
+              onChange={() => handleCheckBox(todo.id)}
+            ></input>
+            <button onClick={() => removeData(todo.id)}>Remove</button>
+          </div>
+        </div>
+      ))}
+      <div className="btns">
+        {todos.length >= 1 && (
+          <button className="remove" onClick={() => setTodos([])}>
+            Remove All
+          </button>
+        )}
+        <button
+          clasName="complete_task"
+          onClick={handleComplete}
+          disabled={!isAnyCompletedTask}
         >
-          Learn React
-        </a>
-      </header>
+          Complete Task
+        </button>
+        {triggerTask &&
+          todos.map(
+            (todo) =>
+              todo.completed && (
+                <div key={todo.id} className="list">
+                  <div className="todolist">{todo.text}</div>
+                </div>
+              )
+          )}
+      </div>
     </div>
   );
 }
-
 export default App;
